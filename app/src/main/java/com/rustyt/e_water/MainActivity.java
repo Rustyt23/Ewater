1890816a21a9902e18e7982a4075679fb8194ab7
Rustyt23 <rustytrooser@gmail.com>
2022-02-02 13:38:17 +0530

    first

---
+++ app/src/main/java/com/rustyt/e_water/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rustyt.e_water;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+     //   val showButton = findViewById<Button>(R.id.showInput);
+    }
+}
